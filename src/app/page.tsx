c70ce05d5e11cc0923c86a04e90d4f78318f127b
Chris <christopher@bridgefund.nl>
2025-02-04 11:50:15 +0100

    feat: filling in information and basic layout / structure

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,99 +1,246 @@
 import Image from "next/image";
 
+const SKILLS = [
+  {
+    name: "Typescript",
+    tags: [ "Language", "Coding" ]
+  },
+  {
+    name: "Javascript",
+    tags: [ "Language", "Coding" ]
+  },
+  {
+    name: "Java",
+    tags: [ "Language", "Coding" ]
+  },
+  {
+    name: "Rust",
+    tags: [ "Language", "Coding" ]
+  },
+  {
+    name: "Python",
+    tags: [ "Language", "Coding" ]
+  },
+  {
+    name: "C",
+    tags: [ "Language", "Coding" ]
+  },
+  {
+    name: "C++ / Cpp",
+    tags: [ "Language", "Coding" ]
+  },
+  {
+    name: "Go",
+    tags: [ "Language", "Coding" ]
+  },
+  {
+    name: "Lua",
+    tags: [ "Language", "Coding" ]
+  },
+  {
+    name: "Postgresql",
+    tags: [ "Database", "System" ]
+  },
+  {
+    name: "Mysql",
+    tags: [ "Database", "System" ]
+  },
+  {
+    name: "Mongodb",
+    tags: [ "Database", "System" ]
+  },
+  {
+    name: "Kubernetes / K8s",
+    tags: [ "Devops", "System" ]
+  },
+  {
+    name: "Docker / DockerCompose",
+    tags: [ "Database", "Technology" ]
+  },
+  {
+    name: "NestJS",
+    tags: [ "Framework", "Backend", "Javascript", "Typescript" ]
+  },
+  {
+    name: "NextJS",
+    tags: [ "Framework", "Frontend", "Javascript", "Typescript" ]
+  },
+  {
+    name: "ReactJS",
+    tags: [ "Framework", "Frontend", "Javascript", "Typescript" ]
+  },
+  {
+    name: "AngularJS",
+    tags: [ "Framework", "Frontend", "Javascript", "Typescript" ]
+  },
+  {
+    name: "VueJS",
+    tags: [ "Framework", "Frontend", "Javascript", "Typescript" ]
+  },
+  {
+    name: "Django",
+    tags: [ "Framework", "Javascript", "Typescript" ]
+  },
+  {
+    name: "Figma",
+    tags: [ "UI-Design", "Mockup" ]
+  },
+  {
+    name: "Sketch",
+    tags: [ "UI-Design", "Mockup" ]
+  },
+]
+
+const Hobbies = [
+  {
+    name: "Coffee",
+    description: "Like most developers I live on coffee, but unlike many my passion for coffee runs deep. I am an active member of my local coffee place. I'll spend hours there just talking about coffee and gear. When I go on holiday I'll bring coffee for us to try out and recommendations for guest roasters. At home I use a flair 58 / V60 / Niche grinder / Acaia scale."
+  },
+  {
+    name: "Concerts",
+    description: "I find it fun and engaging to meet other people and to listen to live music. Often I can be found at small concerts with a group of strangers or friends. My tastes change from time to time, but for now I am into Jazz, Classical, and evidently Heavy Metal."
+  }
+];
+
+const Education = [
+  {
+    source: "University of Manitoba",
+    cert: "B.Sc. Computer Science",
+    logo: "/university-manitoba.png"
+  }
+];
+
 export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
+        <h1 className="text-9x1 font-[family-name:var(--font-geist-mono)]">C. W. Catton</h1>
+        <h2 className="text-9x1 font-[family-name:var(--font-geist-mono)]">Software Engineer</h2>
+        <p>You can call me Chris. I'm a full stack software engineer with a solid education in computer science 
+        and many years in web-development. I'm keen on ai, people, and creating new opportunities for businesses. I 
+        boost a creative mindset and practical strategy for creating workable and extendable initiatives.</p>
+        <section>
+          <h3> Skills </h3>
+          <section>
+            <h4> Languages + Markup </h4>
+            <ul className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
+              {
+                SKILLS.filter(x => x.tags.includes("Language") && x.tags.includes("Coding")).map(skill => (
+                  <li className="mb-2" key={skill.name}>
+                    {skill.name}
+                  </li>
+                ))
+              }
+            </ul>
+          </section>
+          <section>
+            <h4> Databases </h4>
+            <ul className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
+              {
+                SKILLS.filter(x => x.tags.includes("Database") && x.tags.includes("System")).map(skill => (
+                  <li className="mb-2" key={skill.name}>
+                    {skill.name}
+                  </li>
+                ))
+              }
+            </ul>
+          </section>
+          <section>
+            <h4> Skills / Practices </h4>
+            <ul className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
+              {
+                SKILLS.filter(x => x.tags.includes("Development-Practice")).map(skill => (
+                  <li className="mb-2" key={skill.name}>
+                    {skill.name}
+                  </li>
+                ))
+              }
+            </ul>
+          </section>
+          <section>
+            <h4> Skills / Design </h4>
+            <ul className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
+              {
+                SKILLS.filter(x => x.tags.includes("UI-Design")).map(skill => (
+                  <li className="mb-2" key={skill.name}>
+                    {skill.name}
+                  </li>
+                ))
+              }
+            </ul>
+          </section>
+        </section>
+        <section>
+          <h3> Hobbies </h3>
+          <ul className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
+            {
+              Hobbies.map(skill => (
+                <li className="mb-2" key={skill.name}>
+                  {skill.name}
+                  <details> {skill.description} </details>
+                </li>
+              ))
+            }
+          </ul>
+        </section>
+        <section>
+          <h3> Eductation </h3>
+          <ul className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
+            {
+              Education.map(edu => (
+                <li className="flex items-center space-x-4 p-4 shadow-md rounded-lg" key={edu.source}>
+                    <div className="w-16 h-16 relative">
+                      <Image
+                        aria-hidden
+                        src="/university_manitoba.png"
+                        alt={`${edu.source} logo`}
+                        layout="fill"
+                        objectFit="contain"
+                        className="rounded-md"
+                      />
+                    </div>
+                    <div>
+                      {edu.source}, {edu.cert}
+                    </div>
+                </li>
+              ))
+            }
+          </ul>
+        </section>
+        <section>
+          <h3> Projects </h3>
+        </section>
       </main>
       <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
         <a
           className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+          href="https://www.linkedin.com/in/chriswcatton/"
           target="_blank"
           rel="noopener noreferrer"
         >
           <Image
             aria-hidden
-            src="/file.svg"
+            src="/linkedin.png"
             alt="File icon"
             width={16}
             height={16}
           />
-          Learn
+          LinkedIn
         </a>
         <a
           className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+          href="https://github.com/ChrisWC"
           target="_blank"
           rel="noopener noreferrer"
         >
           <Image
             aria-hidden
-            src="/window.svg"
-            alt="Window icon"
+            src="/github-mark-white.svg"
+            alt="File icon"
             width={16}
             height={16}
           />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
+          Github
         </a>
       </footer>
     </div>
