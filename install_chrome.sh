43925c8c17b0edcd1f5d6e801878d7e06100cff5
Chris <christopher@bridgefund.nl>
2025-02-05 16:12:33 +0100

    fix: install chrome script

---
+++ install_chrome.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+apt update && apt install -y \
+   chromium-browser \
+   libnss3 \
+   libatk1.0-0 \
+   libx11-xcb1 \
+   libxcomposite1 \
+   libxcursor1 \
+   libxdamage1 \
+   libxext6 \
+   libxfixes3 \
+   libxi6 \
+   libxrandr2 \
+   libxrender1 \
+   libasound2 \
+   libpango1.0-0 \
+   libjpeg-dev \
+   libxshmfence1 \
+   fonts-liberation \
+   libgbm-dev
+
